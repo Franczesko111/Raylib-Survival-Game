9aec41ebf5d1f9729be0e0924e97d0a5e023ddf3
Franczesko111 <franczeskostr@gmail.com>
2024-08-18 07:40:30 +0200

    Raylib setup
    Window!!!

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(1280, 720, "Game Test");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
